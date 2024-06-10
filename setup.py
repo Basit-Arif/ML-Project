6a1c70bd1fee33c73eb151e800b01dae7b18b78e
basit arif <basitarif@basits-MacBook-Pro.local>
2024-06-10 15:13:13 +0500

    setup added

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
 name='mlproject',
 version='0.0.1',
-author='Krish',
-author_email='krishnaik06@gmail.com',
+author='Abdul Basit',
+author_email='basitarif235@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
